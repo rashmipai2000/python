f325cf5397e39bb268a9df9dd47fcfeb20fe709a
rashmipai2000 <51941008+rashmipai2000@users.noreply.github.com>
2019-06-18 09:53:05 +0530

    Add files via upload
    a string is given as"a=b,c=d,e=f,g=h"
    it should be made into tuples of[(a,b),(c,d),(e,f),(g,h)]
    we use split and then convert to tuple

---
+++ tuple.py
@@ -0,0 +1,21 @@
+def inputval():
+    x="a=b,c=d,e=f,g=h"
+    return x
+def compute(x):
+    m=x.split(",")
+    for i in range(len(m)):
+        m[i]=m[i].split("=")
+    for i in range(len(m)):
+        m[i]=tuple(m[i])
+    return m   
+def output(m):#pass parameters to output function
+    print(m)
+def main():
+    x=inputval()#assign the call of a function to a variable
+    m=compute(x)
+    c=output(m)
+main()  #no need to assign and no parameter in main function  
+
+
+
+    
